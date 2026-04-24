b09b2161b41b90834aa2956cde91234b8a8ae5b8
omjagtap24-oss <om.jagtap24@mmit.edu.in>
2026-04-24 23:26:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,21 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/crowd")
+def crowd():
+    return jsonify({
+        "crowd_level": random.choice(["Low", "Medium", "High"]),
+        "location": "Temple Area",
+        "suggestion": "Check timing before visiting"
+    })
+
+@app.route("/predict")
+def predict():
+    return jsonify({
+        "best_time": "6 PM - Low Crowd",
+        "advice": "Visit during evening hours"
+    })
+
+if __name__ == "__main__":
+    app.run()
