b478655fc25ff8b061587aed09a9373ebfe9ebfb
omjagtap24-oss <om.jagtap24@mmit.edu.in>
2026-04-24 21:39:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,6 @@ import random
 
 app = Flask(__name__)
 
-# Home route (loads webpage)
 @app.route("/")
 def home():
     return render_template("index.html")
-
-# Crowd API
-@app.route("/crowd")
-def crowd():
-    return jsonify({
-        "crowd_level": random.choice(["Low", "Medium", "High"]),
-        "location": "Temple Area",
-        "suggestion": "Check timing before visiting"
-    })
-
-# Prediction API
-@app.route("/predict")
-def predict():
-    return jsonify({
-        "best_time": "6 PM - Low Crowd",
-        "advice": "Visit during evening hours"
-    })
-
-if __name__ == "__main__":
-    app.run()
