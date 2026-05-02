e88579cb29de716d98a90a062a77d783af085c6b
omjagtap24-oss <om.jagtap24@mmit.edu.in>
2026-05-03 00:14:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 import random
+import requests
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
-# Temporary user storage
+# Temporary storage
 users = {}
+search_history = []
 
-# ---------------- SPLASH SCREEN ----------------
+# ---------------- SPLASH ----------------
 @app.route("/")
 def splash():
     return render_template("login.html")
@@ -32,26 +35,66 @@ def login():
     password = request.form.get("password")
 
     if username in users and users[username] == password:
+        session["user"] = username
         return redirect(url_for("home"))
     else:
         return "Invalid Username or Password"
 
 
-# ---------------- FORGOT PASSWORD ----------------
-@app.route("/forgot", methods=["POST"])
-def forgot():
-    username = request.form.get("username")
-
-    if username in users:
-        return "Your password is: " + users[username]
-    else:
-        return "User not found"
+# ---------------- LOGOUT ----------------
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect("/")
 
 
 # ---------------- HOME ----------------
 @app.route("/home")
 def home():
+    if "user" not in session:
+        return redirect("/")
     return render_template("index.html")
+
+
+# ---------------- SEARCH TEMPLE ----------------
+@app.route("/search_temple")
+def search_temple():
+    query = request.args.get("query")
+
+    url = f"https://nominatim.openstreetmap.org/search?q={query} temple india&format=json"
+
+    headers = {
+        "User-Agent": "PilgrimFlowApp"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    results = []
+
+    for place in data[:5]:
+        results.append({
+            "name": place["display_name"],
+            "lat": place["lat"],
+            "lon": place["lon"]
+        })
+
+    return jsonify(results)
+
+
+# ---------------- SAVE SEARCH ----------------
+@app.route("/save_search/<name>")
+def save_search(name):
+    if name not in search_history:
+        search_history.insert(0, name)
+
+    return "saved"
+
+
+# ---------------- HISTORY ----------------
+@app.route("/history")
+def history():
+    return jsonify(search_history[:5])
 
 
 # ---------------- CROWD ----------------
@@ -92,4 +135,4 @@ def predict(temple):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
